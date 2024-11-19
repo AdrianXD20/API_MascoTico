ff37c5f8fec77fd4b4e27a4231f867ec34535a8c
AdrianXDE20 <adricarav0528@gmail.com>
2024-11-19 17:48:34 -0600

    Cambio conexion de BD

--- database/conexion.js
+++ database/conexion.js
@@ -1,18 +1,15 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+// Crear el pool de conexiones
+const pool = mysql.createPool({
     host: process.env.db_host,
     user: process.env.db_user,
     password: process.env.db_password,
     database: process.env.db_name,
+    waitForConnections: true,
+    connectionLimit: 2000, 
+    queueLimit: 0,       
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        throw err;
-    }
-    console.log('Conexión exitosa a la base de datos');
-});
-
-module.exports = db;
+// Exportar el pool
+module.exports = pool.promise(); // Usar promesas para manejar la conexión
